191b3b6b58596cebb1e95cec3cc604010cfb09a3
Alex Sin <alex.sin@cadmus.io>
2026-03-28 15:56:23 +0700

    (feat) test evaluation and translation logic using __main__

--- src/prop_evaluator/components/ast/prop_logic.py
+++ src/prop_evaluator/components/ast/prop_logic.py
@@ -24,6 +24,34 @@ class PropFalse(PropExpression):
     def prefix(self) -> str:
         return "f"
 
+# internal nodes
 
+class PropAnd(PropExpression):
+    def __init__(self, left: PropExpression, right: PropExpression) -> None:
+        self.left = left
+        self.right = right
 
-    
+    def run(self) -> bool:
+        return self.left.run() and self.right.run()
+
+    def prefix(self) -> str:
+        return f"∧ {self.left.prefix()} {self.right.prefix()}"
+
+class PropOr(PropExpression):
+    def __init__(self, left: PropExpression, right: PropExpression) -> None:
+        self.left = left
+        self.right = right
+
+    def run(self) -> bool:
+        return self.left.run() or self.right.run()
+
+    def prefix(self) -> str:
+        return f"v {self.left.prefix()} {self.right.prefix()}"
+
+if __name__ == "__main__":
+    # t ∨ f ∧ f  →  t ∨ (f ∧ f)  =  True
+    # this is explicitly defined to enforce AND precedence over OR.
+    # lexer would be doing it for us in the main app. :)
+    expr = PropOr(PropTrue(), PropAnd(PropFalse(), PropFalse()))
+    print(expr.run()) ## True
+    print(expr.prefix()) ## v t ∧ f f
